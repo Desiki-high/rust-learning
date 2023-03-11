dcbac36776a4b3b7c522d6ecf3cfed281deb2497
Desiki-high <2448906309@qq.com>
2023-03-11 15:28:30 +0800

    create match guard
    Signed-off-by: Desiki-high <2448906309@qq.com>

--- learn/src/pattern/mod.rs
+++ learn/src/pattern/mod.rs
@@ -1,9 +1,13 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+mod example;
+mod guard;
 mod matchs;
 mod option;
 
 pub fn learn_pattern() {
     matchs::learn_match();
     option::learn_option();
+    example::example();
+    guard::learn_guard();
 }
