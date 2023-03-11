b30caf7cb64db32e08e19e97ee3ae51d2893848d
Desiki-high <2448906309@qq.com>
2023-03-11 12:43:41 +0800

    create the probem2
    Signed-off-by: Desiki-high <2448906309@qq.com>

--- problem/src/lib.rs
+++ problem/src/lib.rs
@@ -1,3 +1,5 @@
+use std::mem::size_of;
+
 #[derive(Debug)]
 struct File {
     _ino: i16,
@@ -13,9 +15,23 @@ pub fn problem1() {
     println!("{:p}", &f1);
     println!("{:p}", &f1._ino);
     println!("{:p}", &f1._data);
-    println!("struct size: {}bytes", std::mem::size_of::<File>());
-    println!("i16 size: {}bytes", std::mem::size_of::<i16>());
-    println!("String size: {}bytes", std::mem::size_of::<String>());
+    println!("struct size: {}bytes", size_of::<File>());
+    println!("i16 size: {}bytes", size_of::<i16>());
+    println!("String size: {}bytes", size_of::<String>());
+}
+
+/// problem2: Rust Struct 数组切片和数组引用的区别
+/// ```
+pub fn problem2() {
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let slice: &[i32] = &a[1..3]; //这是一个切片，包含指向切片的首位元素的ptr以及切片长度isize
+    let r: &[i32; 5] = &a; //这是一个数组的引用 包含一个ptr
+    println!("{:p}", &slice);
+    println!("{:p}", &r);
+    dbg!(size_of::<i64>());
+    dbg!(size_of::<&[i32]>());
+    dbg!(size_of::<&[i32; 5]>());
+    dbg!(size_of::<&String>());
 }
 
 #[cfg(test)]
@@ -23,9 +39,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn problem() {
-        println!("Test Start!");
+    fn problem_1() {
         problem1();
-        println!("Test End!");
+    }
+
+    #[test]
+    fn problem_2() {
+        problem2();
     }
 }
