552d901b8f71029159980644faae3f5529620885
Desiki-high <2448906309@qq.com>
2023-03-12 13:48:54 +0800

    update method
    Signed-off-by: Desiki-high <2448906309@qq.com>

--- learn/src/method.rs
+++ learn/src/method.rs
@@ -1,7 +1,18 @@
+#![allow(unused)]
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn new(w: u32, h: u32) -> Rectangle {
+        Rectangle {
+            width: w,
+            height: h,
+        }
+    }
 }
 
 impl Rectangle {
@@ -10,14 +21,43 @@ impl Rectangle {
     }
 }
 
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 pub fn learn_method() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
+    let rect1 = Rectangle::new(30, 50);
+    let rect2 = Rectangle::new(10, 40);
+    let rect3 = Rectangle::new(60, 45);
 
     println!(
-        "The area of the rectangle is {} square pixels.",
+        "The area of the rectangle1 is {} square pixels.",
         rect1.area()
     );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        match self {
+            Message::Quit => println!("Quit"),
+            Message::Move { x, y } => println!("Plese move to ({},{})", x, y),
+            Message::Write(s) => println!("Wirte: {}", s),
+            Message::ChangeColor(r, g, b) => println!("Please change color to ({},{},{})", r, g, b),
+        }
+    }
 }
