699ab1577b5b23ec87cd13927cb3eee768e00eb5
Desiki-high <2448906309@qq.com>
2023-03-13 12:19:16 +0800

    add learn/traits/example
    Signed-off-by: Desiki-high <2448906309@qq.com>

---
+++ learn/src/traits/tarit_example.rs
@@ -0,0 +1,95 @@
+use std::fmt;
+use std::fmt::Display;
+use std::ops::Add;
+
+// 为Point结构体派生Debug特征，用于格式化输出
+#[derive(Debug)]
+struct Point<T: Add<T, Output = T>> {
+    //限制类型T必须实现了Add特征，否则无法进行+操作。
+    x: T,
+    y: T,
+}
+
+impl<T: Add<T, Output = T>> Add for Point<T> {
+    type Output = Point<T>;
+
+    fn add(self, p: Point<T>) -> Point<T> {
+        Point {
+            x: self.x + p.x,
+            y: self.y + p.y,
+        }
+    }
+}
+
+fn add<T: Add<T, Output = T>>(a: T, b: T) -> T {
+    a + b
+}
+
+pub fn learn_tarit_example() {
+    //为自定义类型实现 + 操作
+    let p1 = Point {
+        x: 1.1f32,
+        y: 1.1f32,
+    };
+    let p2 = Point {
+        x: 2.1f32,
+        y: 2.1f32,
+    };
+    println!("{:?}", add(p1, p2));
+
+    let p3 = Point { x: 1i32, y: 1i32 };
+    let p4 = Point { x: 2i32, y: 2i32 };
+    println!("{:?}", add(p3, p4));
+
+    // derive 打印输出
+    let p = Point { x: 3, y: 3 };
+    println!("{:?}", p);
+
+    //自定义类型的打印输出
+    learn_diy_display();
+}
+
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+}
+
+fn learn_diy_display() {
+    let f6 = File::new("f6.txt");
+    println!("{:?}", f6);
+    println!("{}", f6);
+    println!("{}", f6.state);
+}
